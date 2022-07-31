05c96619a29582169305b2eb9264a315fbc9bc88
Howard Ricketts <molorosh@gmail.com>
2022-07-31 13:15:13 +0100

    added interpolated string

--- GflatConsole/Program.fs
+++ GflatConsole/Program.fs
@@ -1,3 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let lang = "F#"
+let greeting = "Hello"
+let emoji = ":-)"
+
+printfn $"{greeting} from {lang} {emoji}"
 
